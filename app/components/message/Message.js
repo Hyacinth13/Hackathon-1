008db39aa16d9fc3a3aa2b6812535a9acd36ca1d
Hyacinth13 <jpeckwriter@gmail.com>
2016-04-09 11:41:30 -0600

    added Message and Messages components

--- app/components/message/Message.js
+++ app/components/message/Message.js
@@ -1,1 +1,81 @@
-Message.js
+import React, {Component} from 'react';
+import $ from 'jquery';
+
+class Message extends Component {
+	constructor(props){
+		super(props);
+		this.toggleEdit = this.toggleEdit.bind(this);
+		this.updateMessage = this.updateMessage.bind(this);
+		this.deleteMessage = this.deleteMessage.bind(this);
+		this.state = {edit:false};
+	}
+
+	toggleEdit() {
+		this.setState({edit: !this.state.edit});
+	}
+
+	updateMessage() {
+		$.ajax({
+			url: `/messages/${this.props._id}`, 
+			type: "PUT", 
+			dataType: "JSON", 
+			data: {title: this.refs.title.value, description: this.refs.description.value, }
+		}).done(message => {
+			this.toggleEdit();
+			this.props.refresh();
+		}).fail(msg=> {
+			console.log(msg);
+	});
+
+	deleteMessage() {
+		$.ajax({
+			url: `/messages/${this.props._id}`,
+			type: "DELETE", 
+			dataType: "JSON", 
+			data: {titel: this. refs.title.value, description: this.refs.description.value, }
+		}).done(message => {
+			this.props.refresh();
+		}).fail(msg => {
+			console.log(msg);
+		});
+	}
+
+	edit(){
+		return(<div className="col s12 m3">
+						 <div className='card purple darken-1'>
+							 <div className='card-content white-text'>
+								 <input required = {true} type = 'text' ref = 'title' placeholder = "Title" defaultValue = {this.props.title} />
+								 <textarea ref = 'description'> this.props.description </textarea>
+							 </div>
+						 </div>
+						</div>
+						<div className='card action'>
+							<button className='btn' onClick = {this.toggleEdit}>Cancel </button>
+							<button className= 'btn' onClick = {this.updateMessage}> Save </button>
+						</div>);
+	}
+	message(){
+		return(<div className="col s12 m3">
+						 <div className='card purple darken-1'>
+							 <div className='card-content white-text'>
+								 <span className='card-title' onClick={this.toggleEdit}> {this.props.title} </span>
+								 <p> {this.props.description || "Click message title to add a description"} </p>
+							 </div>
+							 <div className='card action'>
+								<button className='btn' onClick = {this.deleteMessage}>Delete </button>
+								<a href= { `/messages/${this.props._id}`} className='btn'> Show </a>
+							 </div>
+							</div>
+					  </div>);
+
+	}
+	render() {
+		if(this.state.edit)
+			return this.edit();
+		esle
+		return this.message();
+	}
+	
+}
+
+export default Message 
--- app/components/message/Messages.js
+++ app/components/message/Messages.js
@@ -1,1 +1,48 @@
-Messages.js
+import React, {Component} from 'react';
+import Message from './Message';
+import MessageForm from './MessageForm';
+import $ from 'jquery';
+
+class Messages extends Component {
+	constructor(props){
+		super(props);
+		this.addMessage = this.addMessage.bind(this);
+		this.getMessages = this. getMessages.bind(this);
+		this.state = {messages: [] };
+
+	}
+
+	 getMessages() {
+	 	$.ajax ({
+	 		url: '/messages',
+	 		type: "GET",
+	 		DataType: "JSON",
+	 	}).done(messages => {this.setState({messages: messages});
+	 }).fail(msg =>{console.log(msg)
+	 });
+
+	 componentDidMount(){
+	 	this.getMessages();
+	 }
+
+	 addMessage() {
+	 	this.setState({messages: [...this.state.message, message] });
+	 }
+
+	 }
+	 render(){
+	 	let messages = this.state.messages( message => {
+	 		return(<Message refresh={this.getMessages} key = {`message -${message._id}`} {...message}/>);
+	 	});
+	 		return(
+	 			<div>
+	 				<MessageForm addMessage={this.addMessage}/>
+	 				<div className = 'row'>
+	 					{messages}
+	 				</div>
+	 			</div>
+	 		);
+	 }
+}
+
+export default Messages; 	

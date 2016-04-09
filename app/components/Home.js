4ca46527b289a8cf85671789b622e52d28bb8a52
Patrick Waite <patrick.waite01@gmail.com>
2016-04-09 17:34:34 -0600

    styling

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,11 +3,20 @@ import React, {Component} from 'react';
 class Home extends Component {
 	render() {
   	return ( 
-  		<div>
-        <h1>Community Coms</h1>
-        <a href='/landlord' className='btn'>LandLord Board</a>
-        <a href='/community' className='btn'>Community Board</a>
+  		<div className='parallax-container'>	
+	  		<div className='background'>
+	  			<div className='center-align'>
+	          <h1 className='grey-text text-darken-3'>Select A Message Board</h1>
+	        </div> 
+	        <div className='center-align'>
+	        	<a href='/landlord' className='btn-large card grey darken-3 red-text red-darken-3'>LandLord Board</a>
+	        </div>
+	        <div className='center-align'>	
+	        	<a href='/community' className='btn-large card grey darken-3 red-text red-darken-3'>Community Board</a>
+	        </div>	
+    		</div>
     	</div>
+
     );
 	}
 }
